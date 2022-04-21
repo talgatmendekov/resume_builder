abd9202e63633081e534de2ffbc894d8c93d55fc
talgatmendekov <talgatmendekov@gmail.com>
2022-04-21 16:45:07 +0600

    overal optimization of the project and add README.md

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../assets/logo.svg'
+import logo from '../assets/icons/logo.svg'
 import thumbn from '../assets/template.png'
 import Button from './UI/Button'
 import { Link } from 'react-router-dom'
